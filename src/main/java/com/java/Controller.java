007ca62a689f77dd9066c22faaae3f0c5f687058
Liena Bondarenko <Liena_Bondarenko@epam.com>
2017-09-27 17:40:09 +0300

    I'm going home

--- src/main/java/com/java/Controller.java
+++ src/main/java/com/java/Controller.java
@@ -1,4 +1,5 @@
 package com.java;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +16,21 @@ public class Controller {
         this.model = model;
         this.view = view;
     }
+
+    public boolean checkWithRegExp(String userString){
+        Pattern p = Pattern.compile("^[a-z0-9_-]{3,15}$");
+        Matcher m = p.matcher(userString);
+        System.out.println(View.INPUT);
+
+//        do {
+//            in = new Scanner(System.in);
+//        }
+//        while ();
+
+        return m.matches();
+    }
+
+
 
 
 }
--- src/main/java/com/java/View.java
+++ src/main/java/com/java/View.java
@@ -1,15 +1,22 @@
 package com.java;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by Liena_Bondarenko on 9/27/2017.
  */
 public class View {
+    public static final String INPUT = "New contact adding";
     public static final String INPUT_FNAME = "Please input name: ";
     public static final String INPUT_LNAME = "Please input surname: ";
     public static final String INPUT_NICKNAME = "Please input nickname: ";
     public static final String INPUT_COMMENT = "Please input comment: ";
 
-    private static final String REGEX_NAME = "[A-Za-z]{2,20}";
-    private static final String REGEX_COMMENT = "[A-Za-z]{2,20}";
-    private static final String REGEX_PHONE_NUMBER = "";
+    //private static final String REGEX_NAME = "[A-Za-z]{2,20}";
+    //private static final String REGEX_COMMENT = "[A-Za-z]{2,20}";
+    //private static final String REGEX_PHONE_NUMBER_MOBILE = "";
+    Pattern REGEX_NAME = Pattern.compile("^[A-Za-z0-9_-]{2,15}$");
+    Pattern REGEX_COMMENT = Pattern.compile("^[A-Za-z0-9_-]{2,15}$");
+    Pattern REGEX_PHONE_NUMBER_MOBILE = Pattern.compile("^[0-9]{10,15}$");
+
 }
